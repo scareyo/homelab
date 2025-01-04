bbced7ce84540071378dd0c7bee250a2a5e9f6c3
Samuel Carey <sam@scarey.me>
2025-01-04 05:53:59 -0500

    Create Talos cluster (#1)
    * Remove terraform module
    
    * wip: create talos cluster
    
    * automate talos bootstrap
    
    * resolve ansible-lint warnings
    
    * switch to nixpkgs release 24.11
    
    * fix multiline commands
    
    * move credentials into a yaml file
    
    * break up playbooks
    
    * add metallb to argocd
    
    * expose argocd service
    
    * switch CNI to cilium
    
    * add kubernetes.core ansible collection

--- terraform/modules/base/storage.tf
+++
@@ -1,8 +0,0 @@
-resource "harvester_storageclass" "infrastructure-longhorn" {
-  name = "infrastructure-longhorn"
-  parameters = {
-    "migratable" = "true"
-    "numberOfReplicas" = "1"
-    "staleReplicaTimeout" = "30"
-  }
-}
