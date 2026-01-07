eac43bf2394b893db983c601078474266312a1b9
Samuel Carey <sam@scarey.me>
2026-01-06 22:42:08 -0500

    chore(deps): update helm release argo-cd to v9.2.4 (#18)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- nix/charts/argo-cd/default.nix
+++ nix/charts/argo-cd/default.nix
@@ -1,6 +1,6 @@
 {
   repo = "https://argoproj.github.io/argo-helm";
   chart = "argo-cd";
-  version = "9.2.3";
-  chartHash = "sha256-veKkKmih3QQd3McrncxDaavRp2aYgoocDrpRTwqpSiE=";
+  version = "9.2.4";
+  chartHash = "sha256-c8rtVYMnaaN4q6TlchzkD/hU7ZFxiM7WextnMpD3of4=";
 }
