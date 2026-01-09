84584944776b5ea868cdfc9514e877a9111e3b14
Samuel Carey <sam@scarey.me>
2026-01-09 15:49:48 -0500

    fix: erroneous space before mariadb-operator versions

--- nix/charts/mariadb-operator/default.nix
+++ nix/charts/mariadb-operator/default.nix
@@ -1,6 +1,6 @@
 {
   repo = "https://helm.mariadb.com/mariadb-operator";
   chart = "mariadb-operator";
-  version = " 25.10.3";
+  version = "25.10.3";
   chartHash = "sha256-D442HJpJOR6x/lTqLmBp7tDKUrko5oOa3xW4dKmA+Tw=";
 }
