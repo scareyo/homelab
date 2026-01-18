2553244cf003ba35f128c0051a98643b7cd4f3c2
Samuel Carey <sam@scarey.me>
2026-01-17 19:06:51 -0500

    chore(deps): update ghcr.io/woodpecker-ci/helm/woodpecker docker tag to v3.5.1 (#42)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- nix/charts/woodpecker/default.nix
+++ nix/charts/woodpecker/default.nix
@@ -1,6 +1,6 @@
 {
   repo = "oci://ghcr.io/woodpecker-ci/helm";
   chart = "woodpecker";
-  version = "3.5.0";
-  chartHash = "sha256-y1B+KDagmZnxt9l0sN6F14COWGFQuNWsIr6qkbA22P0=";
+  version = "3.5.1";
+  chartHash = "sha256-Vpx7NWoAE5wnoVtFuFJZmlFaNbpHtnQA9hlJKkBxPnU=";
 }
