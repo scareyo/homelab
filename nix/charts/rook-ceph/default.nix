810f02c40ad9f57a5b21b11d7566dd4fff9bd7d8
Samuel Carey <sam@scarey.me>
2026-01-17 19:05:42 -0500

    chore(deps): update helm release rook-ceph to v1.18.9 (#37)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- nix/charts/rook-ceph/default.nix
+++ nix/charts/rook-ceph/default.nix
@@ -1,6 +1,6 @@
 {
   repo = "https://charts.rook.io/release";
   chart = "rook-ceph";
-  version = "v1.18.8";
-  chartHash = "sha256-CY0RBCLl9WX4KsI1lpLUKFHvltJs5dCm8Ssyv0ZSlmk=";
+  version = "v1.18.9";
+  chartHash = "sha256-QfCG/3iW2UqiWAxIJ6VVI2LraOg3n+v6Ee0I1DTD2U8=";
 }
