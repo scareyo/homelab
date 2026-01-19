ac090ec019b8052adec7ed94ffba1693a826c0b1
Samuel Carey <sam@scarey.me>
2026-01-19 15:05:11 -0500

    chore(deps): update helm release homarr to v8.9.1 (#33)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- nix/charts/homarr/default.nix
+++ nix/charts/homarr/default.nix
@@ -1,6 +1,6 @@
 {
   repo = "https://homarr-labs.github.io/charts";
   chart = "homarr";
-  version = "8.8.1";
-  chartHash = "sha256-giNJn8C8kCF2XaHR+A2dXMFzUoEd9QdI/CHZKOwYDy4=";
+  version = "8.9.1";
+  chartHash = "sha256-o7AhzsIINze96wOhj6T/ETmiE7t3qOczpNxKzE5cHfs=";
 }
