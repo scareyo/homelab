ecd0920b7e382a188150e3b89c37f7e35d69a3c8
Samuel Carey <sam@scarey.me>
2026-02-04 01:13:21 -0500

    feat: downgrade nvidia-device-plugin

--- nix/charts/nvidia-device-plugin/default.nix
+++ nix/charts/nvidia-device-plugin/default.nix
@@ -1,6 +1,6 @@
 {
   repo = "https://nvidia.github.io/k8s-device-plugin";
   chart = "nvidia-device-plugin";
-  version = "0.17.1";
-  chartHash = "sha256-kWM1BfIdCtX657fM9u7D6fRgYRmASTAU7FbMFo/zyvQ=";
+  version = "0.13.0";
+  chartHash = "sha256-2F+4tO0XEg4DtcGxuKyMON+h7PKZORJnvOVIOPtRWaw=";
 }
