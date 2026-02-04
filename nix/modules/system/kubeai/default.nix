ddaf6b0d7416a847209d1670328e020b65babb95
Samuel Carey <sam@scarey.me>
2026-02-04 01:54:46 -0500

    feat: add models

--- nix/modules/system/kubeai/default.nix
+++ nix/modules/system/kubeai/default.nix
@@ -20,6 +20,29 @@ in
         chart = charts.kubeai;
         values = {
           secrets.huggingface.name = "huggingface";
+          resourceProfiles = {
+            rtx3080 = {
+              imageName = "nvidia-gpu";
+              nodeSelector = {
+                "kubernetes.io/hostname" = "s-flamingo";
+              };
+              limits."nvidia.com/gpu" = "1";
+              requests = {
+                "nvidia.com/gpu" = "1";
+              };
+              runtimeClassName = "nvidia";
+            };
+          };
+        };
+      };
+
+      helm.releases.models = {
+        chart = charts.kubeai-models;
+        values = {
+          catalog."deepseek-r1-distill-qwen-1.5b-rtx4070" = {
+            enabled = true;
+            resourceProfile = "rtx3080:1";
+          };
         };
       };
 
