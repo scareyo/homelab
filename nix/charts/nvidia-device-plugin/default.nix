3b7c03b8e7d1eb64678d81106b96ea720b8535e2
Samuel Carey <sam@scarey.me>
2026-02-05 17:39:21 -0500

    feat: upgrade nvidia-device-plugin

--- nix/charts/nvidia-device-plugin/default.nix
+++ nix/charts/nvidia-device-plugin/default.nix
@@ -1,6 +1,6 @@
 {
   repo = "https://nvidia.github.io/k8s-device-plugin";
   chart = "nvidia-device-plugin";
-  version = "0.13.0";
-  chartHash = "sha256-2F+4tO0XEg4DtcGxuKyMON+h7PKZORJnvOVIOPtRWaw=";
+  version = "0.18.2";
+  chartHash = "sha256-iamPnjvW4Lksmqyiu+ztBo5fr5uFcJRumcy4GDynEUg=";
 }
--- nix/modules/system/nvidia/default.nix
+++ nix/modules/system/nvidia/default.nix
@@ -2,7 +2,7 @@
 
 let
   cfg = config.vegapunk.nvidia;
-  namespace = "kube-system";
+  namespace = "nvidia";
   project = "system";
 in
 {
@@ -14,14 +14,21 @@ in
     applications.nvidia = {
       inherit namespace project;
 
+      createNamespace = true;
+
       helm.releases.nvdp = {
         chart = charts.nvidia-device-plugin;
         values = {
           runtimeClassName = "nvidia";
+          gfd.enabled = true;
         };
       };
 
       resources = {
+        namespaces.${namespace} = {
+          metadata.labels."pod-security.kubernetes.io/enforce" = lib.mkForce "privileged";
+        };
+
         "node.k8s.io".v1.RuntimeClass.nvidia = {
           handler = "nvidia";
           scheduling.nodeSelector = {
