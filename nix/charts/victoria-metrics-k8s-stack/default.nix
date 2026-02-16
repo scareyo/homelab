7aa216b6a9346679ab0ac268a2e29ecefa7e4146
Samuel Carey <sam@scarey.me>
2026-02-15 21:01:02 -0500

    chore(deps): update helm release victoria-metrics-k8s-stack to v0.70.0 (#66)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- nix/charts/victoria-metrics-k8s-stack/default.nix
+++ nix/charts/victoria-metrics-k8s-stack/default.nix
@@ -1,6 +1,6 @@
 {
   repo = "https://victoriametrics.github.io/helm-charts";
   chart = "victoria-metrics-k8s-stack";
-  version = "0.69.0";
-  chartHash = "sha256-WDX58EMZY2wuKJ0LiMaVHiSdKrdT5khLIbpLtE3RSyU=";
+  version = "0.70.0";
+  chartHash = "sha256-FzWh3/Hkvf9oTBIk3nErdWNXHSTfwjhE9sftvjpHynk=";
 }
