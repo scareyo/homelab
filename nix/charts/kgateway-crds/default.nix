aa394eff6fd4f804b4c6bace47948208c143d6fc
Samuel Carey <sam@scarey.me>
2026-03-14 16:51:01 -0400

    chore(deps): update cr.kgateway.dev/kgateway-dev/charts/kgateway-crds docker tag to v2.2.2 (#65)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- nix/charts/kgateway-crds/default.nix
+++ nix/charts/kgateway-crds/default.nix
@@ -1,6 +1,6 @@
 {
   repo = "oci://cr.kgateway.dev/kgateway-dev/charts";
   chart = "kgateway-crds";
-  version = "v2.1.2";
-  chartHash = "sha256-/k7qvaPrOKP75RCN9wnfMnRKgReV3uwAyx0vjoVL5L4=";
+  version = "v2.2.2";
+  chartHash = "sha256-O2cctZJDQScuFd+7rZZbshrw3O8ar+3QRWt89m54hpI=";
 }
