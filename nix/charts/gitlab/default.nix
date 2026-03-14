b433ae049f085ac1960e1977075d5f8969f03333
Samuel Carey <sam@scarey.me>
2026-03-14 18:14:25 -0400

    chore(deps): update helm release gitlab to v9.9.3 (#70)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- nix/charts/gitlab/default.nix
+++ nix/charts/gitlab/default.nix
@@ -1,6 +1,6 @@
 {
   repo = "https://charts.gitlab.io";
   chart = "gitlab";
-  version = "9.8.2";
-  chartHash = "sha256-hBhmj0s3OcqBh/xaSbyOSbaxvFdY3OQuixyQlMokEXo=";
+  version = "9.9.3";
+  chartHash = "sha256-uUb0DwnDD8sVMjKm88BWrXRklSoR8jVyxWioZN914AU=";
 }
