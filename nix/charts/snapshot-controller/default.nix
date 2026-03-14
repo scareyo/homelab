90913a76029073b55226b9ed1af08010cbf432f6
Samuel Carey <sam@scarey.me>
2026-03-14 18:27:10 -0400

    chore(deps): update helm release snapshot-controller to v5.0.3 (#80)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- nix/charts/snapshot-controller/default.nix
+++ nix/charts/snapshot-controller/default.nix
@@ -1,6 +1,6 @@
 {
   repo = "https://piraeus.io/helm-charts";
   chart = "snapshot-controller";
-  version = "5.0.2";
-  chartHash = "sha256-yUiQE0havQQhahzmw9ACjxfc29bBct8acIjKb+oA7gE=";
+  version = "5.0.3";
+  chartHash = "sha256-U5hKhUJU3OSVu/li40Lf5Et9UaUpj35N3zNch8QnBXQ=";
 }
