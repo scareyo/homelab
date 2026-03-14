8e8e3fabb273513f5979c9591962956177016974
Samuel Carey <sam@scarey.me>
2026-03-14 19:06:22 -0400

    chore(deps): update helm release external-secrets to v2 (#94)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- nix/charts/external-secrets/default.nix
+++ nix/charts/external-secrets/default.nix
@@ -1,6 +1,6 @@
 {
   repo = "https://external-secrets.io";
   chart = "external-secrets";
-  version = "1.3.2";
-  chartHash = "sha256-rjkUkCbVVFB75flx12V517rJzz9GvGZueAON+q7lv+I=";
+  version = "2.1.0";
+  chartHash = "sha256-ppYiiHPYXjsCAji0XvqShfNGmnmq2bkSlLguEjgDXAI=";
 }
