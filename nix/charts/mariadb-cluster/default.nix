db98078da6355effda935c018fe468a18d39134f
Samuel Carey <sam@scarey.me>
2026-03-14 19:06:38 -0400

    chore(deps): update helm release mariadb-cluster to v26 (#95)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- nix/charts/mariadb-cluster/default.nix
+++ nix/charts/mariadb-cluster/default.nix
@@ -1,6 +1,6 @@
 {
   repo = "https://helm.mariadb.com/mariadb-operator";
   chart = "mariadb-cluster";
-  version = "25.10.4";
-  chartHash = "sha256-E6pllm8lrVL5vbsCyaq/Y2C66Y3lpm0jr//GoiIXrKs=";
+  version = "26.3.0";
+  chartHash = "sha256-9frY9/ZiBYMRtAXJr1P9dojAhFnIMJsTKR1LYbI7FWM=";
 }
