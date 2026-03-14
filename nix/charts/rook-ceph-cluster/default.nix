6ba1f32c4074345e4a7db1a57ad64d5b2dde9252
Samuel Carey <sam@scarey.me>
2026-03-14 18:18:15 -0400

    chore(deps): update helm release rook-ceph-cluster to v1.19.2 (#71)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- nix/charts/rook-ceph-cluster/default.nix
+++ nix/charts/rook-ceph-cluster/default.nix
@@ -1,6 +1,6 @@
 {
   repo = "https://charts.rook.io/release";
   chart = "rook-ceph-cluster";
-  version = "v1.19.0";
-  chartHash = "sha256-GOYYxPe7XWycR8L0pABH8i693nJWzo+9nhFx1UcU9Q8=";
+  version = "v1.19.2";
+  chartHash = "sha256-mh2/hX4lh2vFxq75qcknTXXpdm/P8/pUZnbGUkMB0V4=";
 }
