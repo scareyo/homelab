9ab513701981b00e22ca9881417353d562aab20b
Samuel Carey <sam@scarey.me>
2026-04-11 16:41:00 -0400

    chore(deps): update helm release nvidia-device-plugin to v0.19.0 (#107)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- nix/charts/nvidia-device-plugin/default.nix
+++ nix/charts/nvidia-device-plugin/default.nix
@@ -1,6 +1,6 @@
 {
   repo = "https://nvidia.github.io/k8s-device-plugin";
   chart = "nvidia-device-plugin";
-  version = "0.18.2";
-  chartHash = "sha256-iamPnjvW4Lksmqyiu+ztBo5fr5uFcJRumcy4GDynEUg=";
+  version = "0.19.0";
+  chartHash = "sha256-u2f9y8pyNDitrbFwRHift35We4Wk/NS9vyVOnmUTjyM=";
 }
