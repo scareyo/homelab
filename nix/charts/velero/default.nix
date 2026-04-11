f74cafafd9d392e99e771f9cebcd56c39e46b715
Samuel Carey <sam@scarey.me>
2026-04-11 16:38:36 -0400

    chore(deps): update helm release velero to v12 (#101)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- nix/charts/velero/default.nix
+++ nix/charts/velero/default.nix
@@ -1,6 +1,6 @@
 {
   repo = "https://vmware-tanzu.github.io/helm-charts";
   chart = "velero";
-  version = "11.4.0";
-  chartHash = "sha256-8PrcU3pX02Q/z6UeffM1sJckSSC13TBIE8gwvAs94Ss=";
+  version = "12.0.0";
+  chartHash = "sha256-8dG73K3/3DtGECV494JmzhlxSnv+HqM68xjRmIvbKaU=";
 }
