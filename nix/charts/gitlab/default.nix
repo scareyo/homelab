5905a1e9a616f8f7e084dce6b5de9574f8c59694
Samuel Carey <sam@scarey.me>
2026-04-13 02:38:06 -0400

    feat: remove gitlab and homarr

--- nix/charts/gitlab/default.nix
+++
@@ -1,6 +0,0 @@
-{
-  repo = "https://charts.gitlab.io";
-  chart = "gitlab";
-  version = "9.10.3";
-  chartHash = "sha256-8tmFE5cQcRoQrFB5GEuOS/pjUCFjPyM7RvzgXqF3dw0=";
-}
--- nix/charts/homarr/default.nix
+++
@@ -1,6 +0,0 @@
-{
-  repo = "https://homarr-labs.github.io/charts";
-  chart = "homarr";
-  version = "8.17.0";
-  chartHash = "sha256-wKfoPUvTIRAIvzdCTELA4AfDrTSPcufcYnCZq5hPxzw=";
-}
