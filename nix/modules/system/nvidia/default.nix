0ebe3879f6d1298dcee028f13cf27a451b9c5e0d
Samuel Carey <sam@scarey.me>
2026-04-13 03:19:20 -0400

    fix: fix nvidia

--- nix/modules/system/nvidia/default.nix
+++ nix/modules/system/nvidia/default.nix
@@ -20,6 +20,11 @@ in
         chart = charts.nvidia-gpu-operator;
         values = {
           driver.enabled = false;
+          toolkit.enabled = false;
+          #toolkit.env = [ 
+          #  { name = "CONTAINERD_CONFIG"; value = "/var/lib/rancher/k3s/agent/etc/containerd/config.toml"; }
+          #  { name = "CONTAINERD_SOCKET"; value = "/run/k3s/containerd/containerd.sock"; }
+          #];
           dcgmExporter.serviceMonitor.enabled = true;
           operator = {
             repository = "dev.vegapunk.cloud/scarey";
@@ -30,6 +35,9 @@ in
             repository = "dev.vegapunk.cloud/scarey";
             image = "gpu-operator";
             version = "4485e9d4b589089370819bddb2cfa396cdd12b78";
+            driver.env = [
+              { name = "DISABLE_DEV_CHAR_SYMLINK_CREATION"; value = "true"; }
+            ];
           };
         };
       };
