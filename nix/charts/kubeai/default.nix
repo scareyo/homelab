21d315cf6f90533a02a12c7b5f649b4528188428
Samuel Carey <sam@scarey.me>
2026-04-12 21:34:56 -0400

    chore(deps): update helm release kubeai to v0.23.2 (#116)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- nix/charts/kubeai/default.nix
+++ nix/charts/kubeai/default.nix
@@ -1,6 +1,6 @@
 {
   repo = "https://www.kubeai.org";
   chart = "kubeai";
-  version = "0.23.1";
-  chartHash = "sha256-vvfQ4WNk6oeRoW7O551pQhgn2APENaPtO6hsLKxX/Do=";
+  version = "0.23.2";
+  chartHash = "sha256-FODL13bRNvFzEq9UBepfT4JalvZckqq1gGqrH5RPF0w=";
 }
