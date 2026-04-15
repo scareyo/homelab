6cd4b7e675ce2dd401d57a6b54a48ab44546d6fe
Samuel Carey <sam@scarey.me>
2026-04-14 21:18:39 -0400

    chore(deps): update helm release prometheus-operator-crds to v28 (#130)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- nix/charts/prometheus-operator-crds/default.nix
+++ nix/charts/prometheus-operator-crds/default.nix
@@ -1,6 +1,6 @@
 {
   repo = "https://prometheus-community.github.io/helm-charts";
   chart = "prometheus-operator-crds";
-  version = "27.0.1";
-  chartHash = "sha256-x92WkaomeSbnSz4AbLdhxZoTCPRyhqERZvQU1IAIfMI=";
+  version = "28.0.1";
+  chartHash = "sha256-28f41nnH7qoPIU1UJx2iyxvdVpixp3e7IwAuxI7Gk4Q=";
 }
