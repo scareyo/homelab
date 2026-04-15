024af9eff5972a689d5639f0c8f72c92622ee002
Samuel Carey <sam@scarey.me>
2026-04-14 21:20:17 -0400

    chore(deps): update helm release victoria-logs-collector to v0.2.16 (#137)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- nix/charts/victoria-logs-collector/default.nix
+++ nix/charts/victoria-logs-collector/default.nix
@@ -1,6 +1,6 @@
 {
   repo = "https://victoriametrics.github.io/helm-charts";
   chart = "victoria-logs-collector";
-  version = "0.2.15";
-  chartHash = "sha256-9L2yFHvGZUOC//by5Ew5hX3r2uac/gwZm2neM/ChbSU=";
+  version = "0.2.16";
+  chartHash = "sha256-wvnDrbM4x6hoMGNWvwkP/1KeawSr4Y2cbP6QAVf7Uq8=";
 }
