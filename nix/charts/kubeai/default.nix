f47552586e6c70f6835c2e9ce8bcdf96cc0e3197
Samuel Carey <sam@scarey.me>
2026-06-20 02:09:15 -0400

    feat: remove s-flamingo gpu node

--- nix/charts/kubeai/default.nix
+++
@@ -1,6 +0,0 @@
-{
-  repo = "https://www.kubeai.org";
-  chart = "kubeai";
-  version = "0.23.2";
-  chartHash = "sha256-FODL13bRNvFzEq9UBepfT4JalvZckqq1gGqrH5RPF0w=";
-}
--- nix/charts/nvidia-device-plugin/default.nix
+++
@@ -1,6 +0,0 @@
-{
-  repo = "https://nvidia.github.io/k8s-device-plugin";
-  chart = "nvidia-device-plugin";
-  version = "0.19.0";
-  chartHash = "sha256-u2f9y8pyNDitrbFwRHift35We4Wk/NS9vyVOnmUTjyM=";
-}
